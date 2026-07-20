b0dc8869109543d2ce6b153909bb1ee20b1a0bc1
Sanjana <sanjana.sureshkumar05@gmail.com>
2026-07-21 04:00:23 +0530

    added marks.py function

--- marks.py
+++ marks.py
@@ -1,1 +1,12 @@
+name = input("Enter student name : ")
+roll_no = input("Enter roll no. : ")
+
+eng = input("English : ")
+math = input("Math : ")
+cse = input("Computer Science : ")
+phy = input("Physics : ")
+
+avg = (int(eng)+int(math)+int(cse)+int(phy))/4
+
+print(f"Percentage = {avg}:.2f")
 
